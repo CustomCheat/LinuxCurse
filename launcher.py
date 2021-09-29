0c7283e8fac0bd5ae0b4376900ebc79c1649aa43
CustomCheat <60106212+CustomCheat@users.noreply.github.com>
2021-09-29 15:02:59 +0200

    added modpack lenght check

--- launcher.py
+++ launcher.py
@@ -1,20 +1,31 @@
 import os
 from pathlib import Path
 from os.path import exists
-# this file launches already installed modpacks
+
 file = open('modpacklist.txt', 'r')
 read = file.read().split("\n")
+if len(read) < 1 or read[0] == "" or read[0] == " " or read[0] == "\n":
+    print("None modpacks installed install modpacks with main.py")
+    exit()
+
 print("Select Modpack: ")
 count = 0
 for modpack in read:
     if exists(modpack + "/") and modpack != "":
         count = count + 1
         print("[" + str(count) + "]" + " " + modpack)
+    else:
+        print("Invalid modpack: " + modpack)
+if count == 0:
+    print("No modpacks found")
+    exit()
 select = input("")
 if int(select) - 1 < count and int(select) > 0:
     modpackname = read[int(select) - 1]
     os.system("minecraft-launcher --workDir " + str(Path().absolute()) + "/" + modpackname + "/.minecraft/")
 else:
     print("Error: Invalid choice")
+
+
 
 
