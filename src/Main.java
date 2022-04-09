9124ba8363e5cc51169d87da4605ddd337f05885
CustomCheat <60106212+CustomCheat@users.noreply.github.com>
2022-04-09 11:18:50 +0000

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.net.URISyntaxException;
-
-public class Main {
-    static StartingFrame starting;
-    public static void main(String[] args) throws URISyntaxException {
-        //search = new SearchFrame();
-        starting = new StartingFrame();
-    }
-}
